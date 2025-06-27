37ceeb3277e517332647a82aace15ac89bede31d
AlexTskhe <tskhealexander@gmail.com>
2025-06-27 11:38:45 +0300

    fix2

--- server/src/routes/userRouter.js
+++ server/src/routes/userRouter.js
@@ -1,8 +1,8 @@
-const Router = require('express').Router();
-const userController = require('../controllers/UserController');
+const userRouter = require('express').Router();
+const UserController = require('../controllers/UserController');
 
 userRouter.post("/signUp", UserController.signup);
 userRouter.post("/login", UserController.login);
 userRouter.get("/logout", UserController.logout);
 
-module.exports = authRouter;
+module.exports = userRouter;
