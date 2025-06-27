5f1ac5fb3146ecc3e3cbd240442594dda1e68c05
SneryJane <snejana.vernaya@gmail.com>
2025-06-27 13:26:40 +0300

    server 1.1

--- server/src/routes/index.router.js
+++ server/src/routes/index.router.js
@@ -2,21 +2,14 @@ const indexRouter = require('express').Router()
 
 const userRouter = require('./userRouter')
 const oceanRouter = require('./ocean.router')
-<<<<<<< HEAD
 const jokeRouter = require('./jokes.router')
+const geographyRouter = require('./geography.router')
 
 indexRouter.use('/user', userRouter)
 indexRouter.use('/ocean', oceanRouter)
 indexRouter.use('/jokes', jokeRouter)
-=======
-const geographyRouter = require('./geography.router')
-
-
-indexRouter.use('/user', userRouter)
-indexRouter.use('/ocean', oceanRouter)
 indexRouter.use('/geography', geographyRouter)
 
->>>>>>> fcdff7232b219c0795f281c8cb994806be6cba6d
 // indexRouter.use((req, res) => {
 //   res.status(404).send('Страница не найдена');
 // });
